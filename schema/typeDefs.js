7a26931859a4f332745223d5112632aacdc9626f
Emil Kais <emil.kais94@gmail.com>
2022-12-19 11:35:38 -0500

    Add property management typeDef/resolver in one sueprgraph

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,21 +1,69 @@
 const typeDefs = `#graphql
-    type Book {
-        title: String
-        author: Author
+    type Renter {
+        id: ID
+        name: String!
+        city: String!
+        rating: Float
+        roommates: [Renter]
     }
 
-    type Author {
+    input CreateRenterInput {
         name: String!
-        books: [Book]
+        city: String!
+        # need ID to attach roommate to renter
+        roommates: [ID]
+    }
+
+    type Property {
+        id: ID
+        name: String!
+        city: String!
+        rating: Float
+        renters: [Renter]
+        available: Boolean
+        description: String
+        photos: [String]
+        propertyOwner: ID!
+    }
+
+    input CreatePropertyInput {
+        name: String!
+        city: String!
+        # need ID to attach renter to renters
+        renters: [ID]
+        available: Boolean
+        description: String
+        photos: [String]
+        propertyOwner: ID!
+    }
+
+    type PropertyOwner {
+        id: ID
+        name: String!
+        address: String!
+        rating: Int
+        properties: [Property]
+        photo: String
+    }
+
+    input CreatePropertyOwnerInput {
+        name: String!
+        address: String!
+        # this ID is optional, maybe propertyOwner created first, then property added later
+        properties: [ID]
+        photo: String
     }
 
     type Query {
-        books: [Book!]!
-        authors: [Author]
+        renters: [Renter]
+        properties: [Property]
+        propertyOwners: [PropertyOwner]
     }
 
     type Mutation {
-        addBook(title: String, author: String): Book
+        createRenter(createRenterInput: CreateRenterInput): Renter
+        createProperty(createPropertyInput: CreatePropertyInput): Property
+        createPropertyOwner(createPropertyOwnerInput: CreatePropertyOwnerInput): PropertyOwner
     }
 `;
 
